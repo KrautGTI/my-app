c8498c7a6dc5c8d8460fc160493318792b9969c7
Douglas James <douglasrcjames@gmail.com>
2020-10-20 15:35:57 -0700

    Added test server target, updated packages,

--- src/Fire.js
+++ src/Fire.js
@@ -16,41 +16,23 @@ const liveConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_LIVE_MEASUREMENT_ID
 };
 
-const config = (liveConfig);
+const testConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_TEST_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_TEST_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_TEST_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_TEST_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_TEST_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_TEST_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_TEST_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_TEST_MEASUREMENT_ID
+};
+
+const config = (process.env.NODE_ENV === 'production' ? liveConfig : testConfig);
 
 const fire = firebase.initializeApp(config);
 const firestore = firebase.firestore();
 const storage = firebase.storage();
 const functions = firebase.functions();
-
-// TODO: if you want in browser notifications, here is the setup for that
-// const auth = firebase.auth();
-// var messaging = null;
-
-// if (firebase.messaging.isSupported()) {
-//   messaging = firebase.messaging();
-//   messaging.usePublicVapidKey(process.env.NODE_ENV === 'production' ? process.env.REACT_APP_FIREBASE_LIVE_VAPID_KEY : process.env.REACT_APP_FIREBASE_TEST_VAPID_KEY);
-// }
-
-// export async function handleTokenRefresh() {
-//   if(auth.currentUser){
-//     return messaging.getToken()
-//       .then((token) => {
-//         firestore.collection("users").doc(auth.currentUser.uid).collection("tokens").doc("push").set({ token: token }, { merge: true })
-//             .then(() => {
-//                 console.log("Successfully updated push notification token.")
-//             })
-//             .catch((error) => {
-//                 console.error("Error setting push notification token: " + error)
-//             })
-//       }).catch((error) => {
-//           console.error("Error getting token: " + error)
-//       })
-//   } else {
-//     return console.log("auth.currentUser is not set, might be signed out")
-//   }
-  
-// }
 
 export { 
   firebase, 
